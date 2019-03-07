f8c77faa0b0bad98196a468043f22c9a7ec135cd
Ryan <ryan@go-text.me>
2019-03-07 08:36:39 -0800

    update

---
+++ work/ignition.py
@@ -0,0 +1,16 @@
+import webbrowser
+
+def run():
+		
+	webbrowser.open("https://techcrunch.com")
+	webbrowser.open("https://gmail.com")
+	webbrowser.open("https://material.io")
+	webbrowser.open("https://calendar.google.com")
+	webbrowser.open("https://github.com/textme/textme-android")
+	webbrowser.open("https://textme.atlassian.net")
+	webbrowser.open("https://analytics.amplitude.com")
+	webbrowser.open("https://fabric.io")
+	webbrowser.open("https://console.firebase.google.com")
+	#webbrowser.open("developer.console.com")
+	webbrowser.open("https://dashboard.textme-app.com")
+run()
