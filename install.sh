0f97c10fb427aa749579edb02c13050f4fb1aa23
Ryan <ryan@go-text.me>
2019-03-07 19:53:21 -0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,100 +1,122 @@
 #!/bin/bash
 
-# get current director
+# get current directory
 export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# backup dotfiles
-echo "Backing up your system's dotfiles..."
-BACKUP_DIR="$HOME/backup_old_dotfiles"
-mkdir $BACKUP_DIR
-mv "~/.bashrc" $BACKUP_DIR
-mv "~/.bash_profile" $BACKUP_DIR
+#********************************
+# Switches
+#********************************
 
+back_up=true
+xcode=false
+homebrew=false
+symlinks=true
+vim=true
+zsh=true
+bash=true
+android=false
 
-echo 'Install xcode'
-sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
-sudo xcodebuild -license
+if [ back_up = true ] ; then
+	echo "*********************************"
+	echo "* BACKUP DOTFILES"
+	echo "*********************************"
+	echo "Backing up your system's dotfiles..."
+	BACKUP_DIR="$HOME/backup_old_dotfiles"
+	mkdir $BACKUP_DIR
+	mv "~/.bashrc" $BACKUP_DIR
+	mv "~/.bash_profile" $BACKUP_DIR
+fi
 
+if [ xcode = true ] ; then	
+	echo "*********************************"
+	echo "* INSTALL XCODE"
+	echo "*********************************"
+	sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+	sudo xcodebuild -license
+fi
 
-echo 'INSTALL HOMEBREW and dependencies'
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if [ homebrew = true ] ; then 
+	echo '*********************************'
+	echo '*INSTALL HOMEBREW and dependencies'
+	echo '*********************************'
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"	
+	echo 'update brew'
+	brew update
+fi
 
+if [ symlinks = true ] ; then	
+	echo "*****************************"
+	echo "* SYMLINKS"
+	echo "*****************************"
+	echo 'creating symlinks'
+	ln -sf "$DOTFILES_DIR/git/.gitconfig" ~
+	ln -sf "$DOTFILES_DIR/git/.gitignore_global" ~
+	ln -sf "$DOTFILES_DIR/zsh/.zshrc" ~
+	ln -sf "$DOTFILES_DIR/zsh/.zsh_exports" ~
+	ln -sf "$DOTFILES_DIR/zsh/.zsh_aliases" ~
+	ln -sf "$DOTFILES_DIR/bash/.bash_profile" ~ 
+	ln -sf "$DOTFILES_DIR/bash/.bashrc" ~
+	ln -sf "$DOTFILES_DIR/vim/.vimrc" ~
+fi
 
-#symlinks
-echo 'creating symlinks'
-ln -sf "$DOTFILES_DIR/git/.gitconfig" ~
-ln -sf "$DOTFILES_DIR/git/.gitignore_global" ~
-ln -sf "$DOTFILES_DIR/zsh/.zshrc" ~
-ln -sf "$DOTFILES_DIR/zsh/.zsh_exports" ~
-ln -sf "$DOTFILES_DIR/zsh/.zsh_aliases" ~
-ln -sf "$DOTFILES_DIR/bash/.bash_profile" ~ 
-ln -sf "$DOTFILES_DIR/bash/.bashrc" ~
-ln -sf "$DOTFILES_DIR/vim/.vimrc" ~
+if [ vim = true ] ; then 
+	echo "*****************************"
+	echo "* Installing MAC VIM and Plugins..."
+	echo "*****************************"
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	vim +PluginInstall +qall
+fi
 
-echo "*****************************"
-echo "Installing MAC VIM and Plugins..."
-echo "*****************************"
+if [ zsh = true ] ; then 
+	echo "*****************************"
+	echo "* Installing ZSH"
+	echo "*****************************"
+	brew install zsh zsh-completions
+	echo "Setting zsh as default shell"
+	chsh -s /bin/zsh
+	echo "*****************************"
+	echo "* oh my zsh"
+	echo "*****************************"
+	# . "$DOTFILES_DIR/install/oh-my-zsh.sh"
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "*****************************"
-echo "Installing ZSH"
-echo "*****************************"
+	# spaceship-prompt (theme)
+	git clone https://github.com/denysdovhan/spaceship-prompt.git "$HOME/.oh-my-zsh/themes/spaceship-prompt"
 
-brew install zsh zsh-completions
+	# powerline fonts
+	brew install fonts-powerline
 
-echo "*****************************"
-echo "Setting zsh as default shell"
-echo "*****************************"
+	# create symlink
+	ln -s "$HOME/.oh-my-zsh/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/themes/spaceship.zsh-theme"
 
-chsh -s /bin/zsh
+	source ~/zsh/.zshrc
+fi
 
-echo "*****************************"
-echo "oh my zsh"
-echo "*****************************"
+if [ bash = true ] ; then 
+	echo "*****************************"
+	echo '* set bash_profile'
+	echo "*****************************"
 
-# . "$DOTFILES_DIR/install/oh-my-zsh.sh"
+	chmod 700 ~/bash/.bash_profile
+	chmod 700 ~/bash/.bashrc
+fi
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ android = true ] ; then
+	echo "*****************************"
+	echo 'install android studio and set sdk path'
+	echo "*****************************"
 
-# spaceship-prompt (theme)
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$HOME/.oh-my-zsh/themes/spaceship-prompt"
+	curl -O "https://dl.google.com/dl/android/studio/install/3.3.2.0/android-studio-ide-182.5314842-mac.dmg"
+	echo 'install flutter and set path'
 
-# powerline fonts
-brew install fonts-powerline
+	curl -O "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.2.1-stable.zip"
+	unzip ~/flutter_macos_v1.2.1-stable.zip
+	export PATH=$PATH:`pwd`/flutter/bin"
+	echo 'update flutter'
+	flutter doctor
+fi
 
-# create symlink
-ln -s "$HOME/.oh-my-zsh/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/themes/spaceship.zsh-theme"
-
-source ~/zsh/.zshrc
-
-echo "*****************************"
-
-echo "*****************************"
-echo 'set bash_profile'
-echo "*****************************"
-
-chmod 700 ~/bash/.bash_profile
-chmod 700 ~/bash/.bashrc
-
-echo "*****************************"
-echo 'install android studio and set sdk path'
-echo "*****************************"
-
-curl -O "https://dl.google.com/dl/android/studio/install/3.3.2.0/android-studio-ide-182.5314842-mac.dmg"
-echo 'install flutter and set path'
-
-curl -O "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.2.1-stable.zip"
-unzip ~/flutter_macos_v1.2.1-stable.zip
-export PATH=$PATH:`pwd`/flutter/bin"
-
-
-echo 'update flutter'
-flutter doctor
-
-echo 'update brew'
-brew update
 
 
